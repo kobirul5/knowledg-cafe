e17cc124a9a23b2573ef531b11ff415aea1490f6
kobirul5 <kobirul05j@gmail.com>
2024-10-21 21:58:20 +0600

    setup vite and react for programming and creat a fake json data and fetch it

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './compnents/Blogs/Blogs'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App

4b96eea4d5c90bf069b2db1c1a429b8424b5c81f
kobirul5 <kobirul05j@gmail.com>
2024-10-22 00:40:07 +0600

    added blog card section and design

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -1,13 +1,32 @@
 import PropTypes from 'prop-types'; 
 const Blog = ({blog}) => {
-    console.log(blog)
+    const {title,cover_img, author_name, author_img, read_time,posted_time,hashtag} = blog
     return (
-        <div>
-            <h1>he</h1>
+        <div className='space-y-3 p-5 border-b'>
+            <div><img className='rounded-lg border' src={cover_img} alt= {`cover picture of title ${title}`} /></div>
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-2'>
+                    <div className=''>
+                    <img className='w-10 h-10 rounded-full' src={author_img} alt="picture of author" />
+                    </div>
+                    <div>
+                        <h2 className='text-2xl font-bold'>{author_name}</h2>
+                        <p className='text-gray-500'>{posted_time}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='text-gray-500'>{read_time}</p>
+                </div>
+            </div>
+           <h1 className='text-5xl font-bold '>{title}</h1>
+           <div>
+            <p className='text-gray-500'>{hashtag}</p>
+            </div>
+            <button className='text-[#6047EC] border-b border-[#6047EC]'>Mark as Read</button>
         </div>
     );
 };
-Blog.prototype = {
-    blog: PropTypes.object
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
 }
 export default Blog;
--- src/compnents/Blogs/Blogs.jsx
+++ src/compnents/Blogs/Blogs.jsx
@@ -10,9 +10,13 @@ const Blogs = () => {
         .then(data=>setBlogs(data))
     },[])
     return (
-        <div className="text-2xl font-bold w-2/3">
-            <h2>Blog:- {blogs.length}</h2>
-            <Blog></Blog>
+        <div>
+            <h2 className="text-2xl font-bold">Blog:- {blogs.length}</h2>
+            <div className=" md:w-2/3">
+            {
+                blogs.map(blog=> <Blog key = {blog.id} blog={blog} ></Blog>)
+            }
+        </div>
         </div>
     );
 };

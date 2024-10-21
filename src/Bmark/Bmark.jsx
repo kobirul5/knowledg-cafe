d5cfcd1d57e1010c52bafff6a617c2451f1d9ac3
kobirul5 <kobirul05j@gmail.com>
2024-10-22 01:57:01 +0600

    bookmark section complete

--- src/Bmark/Bmark.jsx
+++ src/Bmark/Bmark.jsx
@@ -3,7 +3,6 @@ const Bmark = ({bookmark}) => {
 
     return (
         <div>
-
                 <div className='bg-white p-5 rounded-xl '>
                     <h3>{bookmark.title}</h3>
                 </div>
